cb280d385eeffec80c8d6985b767d3bc6440911e
Nikhil K S <nikhilks9547@gmail.com>
2026-08-14 10:45:28 +0530

    Configure NextAuth secret in authOptions

--- frontend/src/lib/authOptions.ts
+++ frontend/src/lib/authOptions.ts
@@ -3,6 +3,7 @@ import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt"
   },
